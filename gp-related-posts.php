004ac698bcb55ae64045ac7a6380f4872dea3328
Jon Mather <carassius@gmail.com>
2019-04-20 12:28:35 +0800

    Update gp-related-posts.php
    Updated version and uri in plugin header

--- gp-related-posts.php
+++ gp-related-posts.php
@@ -1,14 +1,15 @@
 <?php
 /*
 Plugin Name: GP Related Posts
-Plugin URI: https://github.com
+Plugin URI: https://github.com/WestCoastDigital/GP-Related-Posts
 Description: Add related posts to the bottom of your single posts page
-Version: 0.1.0
+Version: 1.0
 Author: Jon Mather
 Author URI: https://github.com/WestCoastDigital
 Text Domain: generatepress
 Domain Path: /languages
 */
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
